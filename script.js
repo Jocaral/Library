85a7e8f9f09229f0bfb8d8f827417a8ed6f81ec3
Jocaral <jocaralito@gmail.com>
2024-10-11 09:39:19 -0500

    creating library

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+
+}
+
+function addBookToLibrary() {
+  // do stuff here
+
+}
