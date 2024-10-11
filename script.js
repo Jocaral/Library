986da4a831cefb71e96aaece4cbe1598bf1f7303
Jocaral <jocaralito@gmail.com>
2024-10-11 18:31:30 -0500

    adding  a button to initiate

--- script.js
+++ script.js
@@ -8,7 +8,32 @@ function Book(title, author, pages) {
 
 }
 
+const btn= document.querySelector('button');
+
+btn.addEventListener('click', () => {
+
 function addBookToLibrary() {
   // do stuff here
+const book1= new Book(prompt('Name'),prompt('Author'), prompt('Pages'));
+// const book1= new Book('jo','joca', 'jocaral');
 
+myLibrary.push(book1);
+
+const bookCard= document.querySelector('.book-card');
+const bookName= bookCard.querySelector('.name');
+const bookAuthor= bookCard.querySelector('.author');
+const bookPage= bookCard.querySelector('.pages');
+
+bookName.textContent=`Title: ${myLibrary[0].title}`;
+bookAuthor.textContent= `Author: ${myLibrary[0].author}`;
+bookPage.textContent= `Pages: ${myLibrary[0].pages}`;
 }
+
+addBookToLibrary();
+})
+
+
+console.log(myLibrary);
+
+
+
