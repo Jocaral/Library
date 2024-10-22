59b53badb0a0346be98715a0a90b8ddb679e3970
Jocaral <jocaralito@gmail.com>
2024-10-22 16:04:18 -0500

    organizing books

--- script.js
+++ script.js
@@ -10,27 +10,64 @@ function Book(title, author, pages) {
 
 const btn= document.querySelector('button');
 
+let count=0;
+
+btn.onclick= function (){
+  ++count;
+  
+}
+
 btn.addEventListener('click', () => {
 
 function addBookToLibrary() {
   // do stuff here
-const book1= new Book(prompt('Name'),prompt('Author'), prompt('Pages'));
-// const book1= new Book('jo','joca', 'jocaral');
+
+
+
+const book1= new Book(prompt('Title'),prompt('Author'), prompt('Pages'));
+
 
 myLibrary.push(book1);
 
-const bookCard= document.querySelector('.book-card');
-const bookName= bookCard.querySelector('.name');
-const bookAuthor= bookCard.querySelector('.author');
-const bookPage= bookCard.querySelector('.pages');
+const containerBooks=document.querySelector('.container-books');
 
-bookName.textContent=`Title: ${myLibrary[0].title}`;
+const bookCard= document.createElement('div');
+bookCard.classList.add("book-card");
+containerBooks.appendChild(bookCard);
+
+const bookTop= document.createElement('div');
+const bookTitle= document.createElement('div');
+const bookAuthor= document.createElement('div');
+const bookPage= document.createElement('div');
+
+bookTop.classList.add("top");
+bookCard.appendChild(bookTop);
+
+bookTitle.classList.add("name");
+bookCard.appendChild(bookTitle);
+
+bookAuthor.classList.add("author");
+bookCard.appendChild(bookAuthor);
+
+bookPage.classList.add("pages");
+bookCard.appendChild(bookPage);
+
+// Print results
+
+
+bookTitle.textContent=`Title: ${myLibrary[0].title}`;
 bookAuthor.textContent= `Author: ${myLibrary[0].author}`;
 bookPage.textContent= `Pages: ${myLibrary[0].pages}`;
+
+
+
+bookTop.textContent=`Book ${count}`;
+
 }
 
+
 addBookToLibrary();
-})
+});
 
 
 console.log(myLibrary);
