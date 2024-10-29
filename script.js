c313f5074c49186911892bf209b610f58d10317f
Jocaral <jocaralito@gmail.com>
2024-10-29 09:42:56 -0500

    recoding

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+
+const createABook = document.getElementById("createABook");
+const newBookDialog = document.getElementById("newBookDialog");
+const outputBox = document.querySelector(".output");
+const confirmBtn = newBookDialog.querySelector("#confirmBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+const form = document.querySelector("form");
+newBookDialog.returnValue = "initialValue"
+
+// open dialog to Add a book
+
+createABook.addEventListener("click", () => {
+  newBookDialog.showModal();
+})
+
+// close dialog When adding a book
+
+cancelBtn.addEventListener("click", (e) => {
+  newBookDialog.close();
+});
+
+//Adding a book
+form.addEventListener("submit", addBookToLibrary);
+
 const myLibrary = [];
 
 function Book(title, author, pages,read) {
@@ -6,56 +30,90 @@ function Book(title, author, pages,read) {
   this.author = author;
   this.pages = pages;
   this.read= read;
-
 }
 
-const createABook = document.getElementById("createABook");
-const newBookDialog = document.getElementById("newBookDialog");
-const outputBox = document.querySelector(".output");
-const confirmBtn = newBookDialog.querySelector("#confirmBtn");
-newBookDialog.returnValue = "initialValue"
+function addBookToLibrary(event) {
+  event.preventDefault();
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  // let statusRead = document.getElementById("Read").checked;
 
-function openCheck(newBookDialog) {
-  if (newBookDialog.open) {
-    outputBox.innerText = "Dialog open";
-  } else {
-    outputBox.innerText = "Dialog closed";
-  }
-};
-function printBook(myLibrary){
-  bookTitle.textContent=`Title: ${myLibrary[0].title}`;
-  bookAuthor.textContent= `Author: ${myLibrary[0].author}`;
-  bookPage.textContent= `Pages: ${myLibrary[0].pages}`;
-  }
+  // let status = statusRead ? "Read" : "Not-Read";
+
+  const newBook = new Book(title, author, pages);
+
+  myLibrary.push(newBook);
+  newBookDialog.close();
+  form.reset();
+
+  displayBooks();
+}
+
+function displayBooks() {
+  outputBox.innerHTML = "";
+  myLibrary.forEach((newbook, index) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    bookCard.innerHTML = `
+        <div class="heading">
+        <h3>Author : ${newbook.author}</h3>
+        <h3>Title : ${newbook.title}</h3>
+        </div>
+
+        <div class="content">
+        <p>Pages : ${newbook.pages}</p>
+       
+</button>
+        </div>
+        
+        `;
+        outputBox.appendChild(bookCard);
+      });
+    }
+
+function remove(index) {
+      myLibrary.splice(index, 1);
+      displayBooks();
+    }
+
+
+// function openCheck(newBookDialog) {
+//   if (newBookDialog.open) {
+//     outputBox.innerText = "Dialog open";
+//   } else {
+//     outputBox.innerText = "Dialog closed";
+//   }
+// };
+// function printBook(myLibrary){
+//   bookTitle.textContent=`Title: ${myLibrary[0].title}`;
+//   bookAuthor.textContent= `Author: ${myLibrary[0].author}`;
+//   bookPage.textContent= `Pages: ${myLibrary[0].pages}`;
+//   }
   
 
-createABook.addEventListener("click", () => {
-  newBookDialog.showModal();
-  openCheck(newBookDialog);
-})
 
-newBookDialog.addEventListener("close", (e) => {
 
-  // const bookTop= document.createElement('div');
-  // function addBookToLibrary(){
-    openCheck(newBookDialog)
-    printBook(myLibrary)
-  } 
+// newBookDialog.addEventListener("close", (e) => {
+//     openCheck(newBookDialog)
+//     printBook(myLibrary)
+//   } 
 
-)
+// )
 
-confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); 
-  const book1= new Book(title.value,author.value, pages.value)
-  myLibrary.push(book1);
-  newBookDialog.close();
-  ;
-  // printBooks();
-});
+// confirmBtn.addEventListener("click", (event) => {
+//   event.preventDefault(); 
+//   const book1= new Book(title.value,author.value, pages.value)
+//   myLibrary.push(book1);
+//   newBookDialog.close();
+//   ;
+//   // printBooks();
+// });
 
-const bookTitle= document.querySelector(".bookTitle");
-const bookAuthor= document.querySelector(".bookAuthor");
-const bookPage= document.querySelector(".bookPage");
+// const bookTitle= document.querySelector(".bookTitle");
+// const bookAuthor= document.querySelector(".bookAuthor");
+// const bookPage= document.querySelector(".bookPage");
 
 
 
